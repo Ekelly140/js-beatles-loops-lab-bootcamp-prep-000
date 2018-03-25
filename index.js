04904e30bcdae707d4ab871f1722bb35aaaeb696
ERIC KELLY <eakelly220@gmail.com>
2018-03-25 18:11:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instuments){
   var newArray=[]
-  for (i=0; i<musicians.length; i++){
+  for (var i=0; i<musicians.length; i++){
     var newString = musicians[i] + " plays "+ instuments[i]
     newArray.push(newString)
   }
