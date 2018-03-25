5df418ec7cbd173ab35fc2f030eae37727997a4b
ERIC KELLY <eakelly220@gmail.com>
2018-03-25 18:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instuments){
   var newArray=[]
   for (i=0; i<musicians.length; i++){
     var newString = musicians[i] + " plays "+ instuments[i]
-    newArray.push[newString]
+    newArray.push(newString)
   }
   return newArray
 }
